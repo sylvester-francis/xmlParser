cbcd3cffd7646aa8ae7bea1766745721d3f409e2
Sylvester Francis <sylvesterranjithfrancis@gmail.com>
2023-10-18 00:14:26 -0400

    User interface methods - refactored

--- xmlparser/parser/user_interface.py
+++ xmlparser/parser/user_interface.py
@@ -11,47 +11,36 @@ last modified date: 10/15/2023
 
 import os,sys
 # Importing functions from other modules
-from product_manipulation import increase_price, rename_category, remove_products, generate_reports
+from product_manipulation import increase_prices, rename_category, remove_products, generate_reports
 from file_operations import save_changes
-from utility_functions import return_file_path
+from utility_functions import return_file_path,return_min_max_rating
 from xml_operations import parse_XML
 
-def quit(products):
-    # Save changes to the XML file before exiting
-    save_changes(products)
-    # Exit the program with status code 1
-    sys.exit(1)
-    
 
-def menu(products):
-    try:
-        # Define menu options as a dictionary
-        menu_options = {
-            "1": "Increase prices",
-            "2": "Rename categories",
-            "3": "Remove products",
-            "4": "Generate report",
-            "5": "Save the file",
-            "6": "Exit"
-        }
-        # Display welcome message and menu options to the user
+class MenuHandler:
+    """Handler for menu actions and external dependencies."""
+    def __init__(self):
+        self.exit_handler = ExitHandler()
+        self.exit_code = 1  # Default exit code
+
+    def display_menu_options(self, menu_options):
         print("**********************************************************************************")
         print("Welcome user")
         print("**********************************************************************************")
         print("Please select an option (Enter a number 1-6)")
-        # Print each menu option with its corresponding number
         for key, value in menu_options.items():
             print(f"{key}:{value}")
-        # Get user input for menu choice
-        choice = input(">  ") 
-        # Check if the user choice is valid
-        if choice not in menu_options:
-            print("Invalid input. Please select a valid option ")
-            return
-        # Execute the chosen action based on user input
+
+    def get_user_choice(self):
+        return input("> ")
+
+    def handle_invalid_choice(self):
+        print("Invalid input. Please select a valid option")
+
+    def execute_menu_choice(self, choice, products):
         if choice == "1":
             print("Increasing prices")
-            updated_products = increase_price(products)
+            updated_products = increase_prices(products)
             if updated_products:
                 save_changes(updated_products)
                 print("Prices increased successfully.")
@@ -88,11 +77,94 @@ def menu(products):
         elif choice == "5":
             print("Saving the file")
             save_changes(products)
-            print("File saved successfully.")    
+            print("File saved successfully.")
         elif choice == "6":
             print("Exiting... Goodbye")
-            quit(products)
-    except Exception as e:
+            self.exit_handler.exit_program(self.exit_code)
+
+    def handle_exception(self, e):
         print(f"An error occurred: {type(e).__name__} - {e}")
+
+class ExitHandler:
+    """Handler for program exit actions."""
+    @staticmethod
+    def exit_program(exit_code=1):
+        sys.exit(exit_code)
+
+def quit(products, exit_handler=None):
+    """Save changes and exit the program."""
+    if exit_handler is None:
+        exit_handler = ExitHandler()
+    save_changes(products)
+    exit_handler.exit_program()   
+
+def menu(products, menu_handler=None):
+    try:
+        if menu_handler is None:
+            menu_handler = MenuHandler()
+
+        menu_options = {
+            "1": "Increase prices",
+            "2": "Rename categories",
+            "3": "Remove products",
+            "4": "Generate report",
+            "5": "Save the file",
+            "6": "Exit"
+        }
+
+        menu_handler.display_menu_options(menu_options)
+        choice = menu_handler.get_user_choice()
+
+        if choice not in menu_options:
+            menu_handler.handle_invalid_choice()
+            return
+
+        if choice == "1":
+            print("Increasing prices")
+            updated_products = increase_prices(products, user_input_function=menu_handler.get_user_choice)
+            if updated_products:
+                save_changes(updated_products)
+                print("Prices increased successfully.")
+            else:
+                save_changes(products)
+                print("No products to update.")
+        elif choice == "2":
+            print("Renaming categories")
+            renamed_products = rename_category(products, user_input_function=menu_handler.get_user_choice)
+            if renamed_products:
+                save_changes(renamed_products)
+                print("Categories renamed successfully.")
+            else:
+                save_changes(products)
+                print("No categories to rename.")
+        elif choice == "3":
+            print("Removing products based on rating")
+            min_rating, max_rating = return_min_max_rating()
+            products_after_removal = remove_products(products, user_input_function=menu_handler.get_user_choice, rating_function=lambda: (min_rating, max_rating))
+            if products_after_removal:
+                save_changes(products_after_removal)
+                print("Products removed successfully.")
+            else:
+                save_changes(products)
+                print("No products to remove.")
+        elif choice == "4":
+            report_filepath = return_file_path()
+            if os.path.isfile(report_filepath):
+                print(f"Generating reports from {report_filepath}")
+                parsed_products = parse_XML(report_filepath)
+                generate_reports(parsed_products)
+            else:
+                print("No changes detected. Generating reports from input file.")
+                generate_reports(products)
+        elif choice == "5":
+            print("Saving the file")
+            save_changes(products)
+            print("File saved successfully.")
+        elif choice == "6":
+            print("Exiting... Goodbye")
+            menu_handler.exit_handler.exit_program(menu_handler.exit_code)
+    except Exception as e:
+        menu_handler.handle_exception(e)
     # Recursive call to keep the menu running
-    menu(products)
+    menu(products, menu_handler)
+
--- xmlparser/parser/utility_functions.py
+++ xmlparser/parser/utility_functions.py
@@ -2,6 +2,8 @@
 This module contains the utility functions
 (1). return_args
 (2). return_min_max_ratings
+(3). get_user_input
+(4). check_file_exists
 
 Author: Sylvester Ranjith Francis
 Date created : 10/15/2023
@@ -10,26 +12,20 @@ last modified date: 10/15/2023
 '''
 import os,argparse
 
+class ArgumentParserWrapper:
+    """Wrapper class for ArgumentParser to enable easier testing."""
+    @staticmethod
+    def create_parser():
+        return argparse.ArgumentParser(description="XML Product Data parser")
+
 
 # Function to parse command-line arguments and return them
-def return_args():
-    # Create an ArgumentParser object
-    parser = argparse.ArgumentParser(description="XML Product Data parser")
-    # Add a positional argument for the file path
-    parser.add_argument('filepath', type=str, help="Please provide the path to the file containing the xml product data")
-    # Parse the command-line arguments
-    args = parser.parse_args()
-    # Return the parsed arguments
-    return args
-def return_file_path():
-    # Initialising a variable directory to point to a directory called output at the root of the project
-    directory = '../output'
-    # Initialising output filename
-    filename = 'outputFile.xml'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    filepath = os.path.join(directory, filename)
-    return filepath
+def return_args(parser=None, args=None):
+    """Parse command-line arguments and return them."""
+    if parser is None:
+        parser = ArgumentParserWrapper.create_parser()
+    parsed_args = parser.parse_args(args)
+    return parsed_args
 
 # Function to return the minimum and maximum rating values
 def return_min_max_rating():
@@ -48,3 +44,13 @@ def check_file_exists(filepath):
     """Check if the specified file exists."""
     if not os.path.isfile(filepath):
         raise FileNotFoundError(f"The specified file is not found: {filepath}")
+
+def return_file_path(directory='../output', filename='outputFile.xml'):
+    try:
+        filepath = os.path.join(directory, filename)
+        check_file_exists(filepath)
+        return filepath
+    except FileNotFoundError as e:
+        raise e
+    except Exception as e:
+        raise FileNotFoundError(f"An exception occurred during file path retrieval: {type(e).__name__} - {e}")
